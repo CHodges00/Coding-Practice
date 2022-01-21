3219d974c8ad453fe57de6c2f927cd18fe6f62ac
Cody Hodges <hodges.cody00@gmail.com>
2022-01-21 12:27:22 -0600

    CodeWars

---
+++ js/external.js
@@ -0,0 +1,30 @@
+
+
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//      a * b
+// }
+/////////////// SOLUTION //////////////
+// function multiply(a, b){
+//     return a * b
+// }
+// console.log(multiply(2,4))
+
+
+// COUNT VOWELS IN STR
+// function getCount(str) {
+//     var vowelsCount = 0;
+//     for(var i = 0; i < str.length; i++){
+//         if ([i].toString() === 'a' || 'e' || 'i' || 'o' || 'u'){
+//             (vowelsCount += 1);
+//         }
+//     }
+//     return vowelsCount;
+// }
+//
+// console.log(getCount('abracadabra'))
+
+
+
+
