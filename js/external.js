43adece0f486652154e934529fb42ae2eff505b4
Cody Hodges <hodges.cody00@gmail.com>
2022-04-30 18:22:34 -0500

    Practice

--- js/external.js
+++ js/external.js
@@ -31,5 +31,14 @@
 // console.log(getCount('abracadabra'))
 //
 
-
+for( let i=1; i<=100; i++ ){
+    if (i%15 === 0){
+        console.log("fizzbuzz")
+    }else if (i%3 === 0){
+        console.log("fizz")
+    }else if (i%5 === 0){
+        console.log("buzz")
+    }else
+        console.log(i)
+}
 
